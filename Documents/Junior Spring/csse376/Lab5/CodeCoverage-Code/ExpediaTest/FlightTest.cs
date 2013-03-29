2b22abc7f63bb2fc9fa99d5d4cc04216bb6c0cb9
Kimberly Boucher <boucheka@rose-hulman.edu>
2013-03-29 17:42:43 -0400

    Submit Lab 5

---
+++ Documents/Junior Spring/csse376/Lab5/CodeCoverage-Code/ExpediaTest/FlightTest.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using NUnit.Framework;
+using Expedia;
+
+namespace ExpediaTest
+{
+    [TestFixture()]
+    class FlightTest
+    {
+        [Test()]
+        public void TestThatFlightInitializes()
+        {
+            DateTime StartDate = new DateTime(2009, 11, 1);
+            DateTime EndDate = new DateTime(2009, 11, 1);
+            var target = new Flight(StartDate, EndDate, 700);
+            Assert.IsNotNull(target);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(InvalidOperationException))]
+        public void TestFlightEndBeforeStart()
+        {
+            DateTime StartDate = new DateTime(2009, 11, 2);
+            DateTime EndDate = new DateTime(2009, 11, 1);
+            var target = new Flight(StartDate, EndDate, 700);
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestFlightNegativeMiles()
+        {
+            DateTime StartDate = new DateTime(2009, 11, 1);
+            DateTime EndDate = new DateTime(2009, 11, 2);
+            var target = new Flight(StartDate, EndDate, -700);
+        }
+
+        [Test()]
+        public void TestFlightEqualsTrue()
+        {
+            DateTime StartDate = new DateTime(2009, 11, 1);
+            DateTime EndDate = new DateTime(2009, 11, 1);
+            var target = new Flight(StartDate, EndDate, 700);
+            var target2 = new Flight(StartDate, EndDate, 700);
+            Assert.True(target.Equals(target2));
+        }
+
+        [Test()]
+        public void TestFlightEqualsHotel()
+        {
+            DateTime StartDate = new DateTime(2009, 11, 1);
+            DateTime EndDate = new DateTime(2009, 11, 1);
+            var target = new Flight(StartDate, EndDate, 700);
+            var target2 = new Car(5);
+            Assert.False(target.Equals(target2));
+        }
+    }
+}
---
+++ Documents/Junior Spring/csse376/Lab5/CodeCoverage-Code/ExpediaTest/HotelTest.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using NUnit.Framework;
+using Expedia;
+
+namespace ExpediaTest
+{
+    [TestFixture()]
+    class HotelTest
+    {
+        private readonly int NightsToRentHotel = 5;
+
+        [Test()]
+        public void TestThatHotelInitializes()
+        {
+            var target = new Hotel(NightsToRentHotel);
+            Assert.IsNotNull(target);
+        }
+
+        [Test()]
+        public void TestThatHotelHasCorrectBasePriceForOneDayStay()
+        {
+            var target = new Hotel(1);
+            Assert.AreEqual(45, target.getBasePrice());
+        }
+
+        [Test()]
+        public void TestThatHotelHasCorrectBasePriceForTwoDayStay()
+        {
+            var target = new Hotel(2);
+            Assert.AreEqual(90, target.getBasePrice());
+        }
+
+        [Test()]
+        public void TestThatHotelHasCorrectBasePriceForTenDaysStay()
+        {
+            var target = new Hotel(10);
+            Assert.AreEqual(450, target.getBasePrice());
+        }
+
+        [Test()]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void TestThatHotelThrowsOnBadLength()
+        {
+            new Hotel(-5);
+        }
+
+        [Test()]
+        public void TestHotelEqualsTrue()
+        {
+            var target = new Hotel(NightsToRentHotel);
+            var target2 = new Hotel(NightsToRentHotel);
+            Assert.True(target.Equals(target2));
+        }
+
+        [Test()]
+        public void TestHotelEqualsFalse()
+        {
+            var target = new Hotel(NightsToRentHotel);
+            var target2 = new Hotel(2*(NightsToRentHotel+1));
+            Assert.False(target.Equals(target2));
+        }
+
+        [Test()]
+        public void TestHotelEqualsCar()
+        {
+            var target = new Hotel(NightsToRentHotel);
+            var target2 = new Car(NightsToRentHotel);
+            Assert.False(target.Equals(target2));
+        }
+
+    }
+}
